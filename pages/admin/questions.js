7cb5f4bcd431dadd90fe9294a82c154ca8e1267a
dulkifal <dulkifilibacker@gmail.com>
2022-12-12 19:24:43 +0530

    delete question set

--- pages/admin/questions.js
+++ pages/admin/questions.js
@@ -1,21 +1,32 @@
 import { useEffect, useState } from "react";
 
-
 const showQuestions = () => {
-  const [questions, setQuestions] = useState([])
-  useEffect(() => {
-   fetchQuestions()
-  },
-  ),[]
-const fetchQuestions = ()=>{
-  fetch("/api/fatwa/ask").then((res)=>res.json()).then((data)=> {
-    setQuestions(data)
-  }
-    )
+  const [questions, setQuestions] = useState([]);
 
-}
- 
-   
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+  
+  const fetchQuestions = () => {
+    fetch("/api/fatwa/ask")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestions(data);
+      });
+  };
+  const deleteIt = (id) => {
+    fetch("/api/fatwa/ask", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({"id": id }),
+    }).then(() => {
+      fetchQuestions();
+    });
+  };
+
+
 
   return (
     <div>
@@ -24,8 +35,11 @@ const fetchQuestions = ()=>{
         {questions.map((question) => (
           <li key={question.id}>
             <h3>{question.question}</h3>
+            <p>{question.answer}</p>
             <p>{question.name}</p>
             <p>{question.email}</p>
+            <button onClick={()=> deleteIt(question.id)}>delete</button>
+
           </li>
         ))}
       </ul>
@@ -34,4 +48,3 @@ const fetchQuestions = ()=>{
 };
 
 export default showQuestions;
-
--- pages/api/fatwa/ask.js
+++ pages/api/fatwa/ask.js
@@ -1,42 +1,77 @@
-import db from "../../../lip/db"
+import db from "../../../lip/db";
 
 export default async function handler(req, res) {
   const { method } = req;
   switch (method) {
-    case 'POST':
-  addQuestion(req, res, db);
+    case "POST":
+      addQuestion(req, res, db);
       break;
-      case 'GET':
-       getQuesions(req, res, db);
-        
+    case "GET":
+      getQuesions(req, res, db);
+      break;
+    case "PATCH":
+      addAnswer(req, res, db);
+      break;
+      case "DELETE":
+        deleteQuestion(req, res, db);
+        break;
+
     // default:
     //   res.setHeader('Allow', ['POST', 'GET']);
     //   res.status(405).end(`Method ${method} Not Allowed`);
-
+  }
 }
-
-}
-async function addQuestion(req, res, db) {
+function addQuestion(req, res, db) {
   const { question, name, email } = req.body;
-  await db.query(
+  db.query(
     `
     INSERT INTO questions (question, name, email)
     VALUES (?, ?, ?)
     `,
-    [question, name, email], (error, results, fields) => {
+    [question, name, email],
+    (error, results, fields) => {
       if (error) throw error;
       res.send(results);
     }
-  )
-  
+  );
 }
 
- function getQuesions(req, res, db) {
-    db.query(
-          `SELECT * FROM questions`,(error, results, fields) => {
-            if (error) throw error;
-            res.send(results);
-          }
-        );
-       
+function getQuesions(req, res, db) {
+  db.query(`SELECT * FROM questions`, (error, results, fields) => {
+    if (error) throw error;
+    res.send(results);
+  });
+}
+
+function addAnswer(req, res, db) {
+  const { answer, id } = req.body;
+  // add answer to the question with id
+  db.query(
+    `
+    UPDATE questions
+    SET answer = ?
+    WHERE id = ?
+    `,
+    [answer, id],
+    
+    (error, results, fields) => {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+}
+
+function deleteQuestion(req, res, db) {
+  const { id } = req.body;
+  db.query(
+    `
+    DELETE FROM questions
+    WHERE id = ?
+    `,
+    [id],
+    (error, results, fields) => {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
 }

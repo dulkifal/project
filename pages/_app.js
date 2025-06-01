abc87b3046c7123a5f0401118ddd668d9f885548
dulkifal <dulkifilibacker@gmail.com>
2025-06-01 11:24:09 +0530

    feat: add meta tags and update icons for improved SEO and UI

--- pages/_app.js
+++ pages/_app.js
@@ -11,11 +11,25 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <LangContext.Provider value={{ lang, setLang }}>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="التفقه - موقع إسلامي شامل" />
+        <meta name="keywords" content="إسلام, فتاوى, مقالات, أحكام, تفقه" />
+        <meta name="author" content="التفقه" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="التفقه - موقع إسلامي شامل" />
+        <meta property="og:description" content="التفقه - موقع إسلامي شامل" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://al-fiqh.vercel.app/" />
+          <link rel="icon" type="image/x-icon" href="/icons/pen-icon.svg" />
+          <title> التفقه </title>
+      </Head>
       <Layout>
         
-          <title> التفقه </title>
-          <meta name="description" content="التفقه" />
-          <lang lang='ar' />
+          
 
         <Component {...pageProps} />
       </Layout>
--- pages/admin/login.js
+++ pages/admin/login.js
@@ -45,7 +45,7 @@ const Login = () => {
           onClick={handleGoogleSignIn}
           className="w-full flex items-center justify-center gap-2 bg-white px-4 py-2 border border-gray-300 rounded-md shadow-sm text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
         >
-          <img src="/google-icon.svg" alt="Google" className="w-6 h-6" />
+          <img src="/icons/google-icon.svg" alt="Google" className="w-6 h-6" />
           <span>تسجيل الدخول باستخدام جوجل</span>
         </button>
 
